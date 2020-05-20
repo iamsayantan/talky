9f31ee76a211582a8f553b270fc588e7fc9b4e40
Sayantan Das <das.sayantan94@gmail.com>
2020-05-20 12:34:38 +0530

    fix in typo

--- client/pages/_room_type/_room_id/index.vue
+++ client/pages/_room_type/_room_id/index.vue
@@ -132,7 +132,7 @@
         this.webrtc.pc = new RTCPeerConnection({
           iceServers: [
             {
-              urls: 'stuns:stun.l.google.com:19302'
+              urls: 'stun:stun.l.google.com:19302'
             }
           ]
         });
