885d2dd2ff5ca35fefd17a8764aa47ad877e3133
Sayantan Das <das.sayantan94@gmail.com>
2020-05-27 11:16:04 +0530

    Refactor of peer connection WIP.

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -8,18 +8,18 @@ module.exports = {
   */
   head: {
     titleTemplate: 'konference - Realtime voice and video chat',
-    title: process.env.npm_package_name || '',
+    title: 'konference - Realtime voice and video chat',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: 'Realtime voice and video chat' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      {
-        rel: 'stylesheet',
-        href:'https://fonts.googleapis.com/css?family=Raleway:400|Roboto+Slab:200&display=swap'
-      }
+      // {
+      //   rel: 'stylesheet',
+      //   href:'https://fonts.googleapis.com/css?family=Raleway:400|Roboto+Slab:200&display=swap'
+      // }
     ],
   },
   server: {
